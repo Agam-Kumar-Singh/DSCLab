2d2bdf59551a58ff174e346107bca970c2c43d91
Agam Kumar Singh <80572287+Agam-Kumar-Singh@users.noreply.github.com>
2023-01-18 22:54:01 +0530

    Update Linear Regression.R

--- Linear Regression.R
+++ Linear Regression.R
@@ -1,3 +1,5 @@
+#TUTORIAL 1:
+
 # x<-c(1,2,3,4,5,6,7,8,9,10)
 # y<-c(11,12,13,14,15,16,17,18,19,20)
 # 
@@ -11,20 +13,51 @@
 # ans<-predict(relation,data[])
 # ans
 
+#TUTORIAL 2:
+# library(ggplot2)
+# library(dplyr)
+
+# data<-read.csv("C:/Users/agams/OneDrive/Desktop/Sem7/DSC Lab/income.data.csv")
+# summary(data)
+# head(data)
+# plot(happiness~income,data)
+
+# linear.regression<-lm(happiness~income,data)
+# summary(linear.regression)
+
+# income.graph<-ggplot(data,aes(x=income,y=happiness))+geom_point()
+# income.graph
+# income.graph<-income.graph+geom_smooth(method="lm")
+# income.graph
+
+# predict(linear.regression,data.frame(income=c(1,2,3)))
+
+#TUTORIAL 3:
 library(ggplot2)
 library(dplyr)
 
 data<-read.csv("C:/Users/agams/OneDrive/Desktop/Sem7/DSC Lab/income.data.csv")
 summary(data)
 head(data)
-plot(happiness~income,data)
 
-linear.regression<-lm(happiness~income,data)
-summary(linear.regression)
 
-income.graph<-ggplot(data,aes(x=income,y=happiness))+geom_point()
-income.graph
-income.graph<-income.graph+geom_smooth(method="lm")
-income.graph
+ggplot(data)+geom_point(aes(x=income,y=happiness))
 
-predict(linear.regression,data.frame(income=c(1,2,3)))
+library(caTools)
+
+split<-sample.split(data,SplitRatio = 0.7)
+
+training<-subset(data,split==TRUE)
+test<-subset(data,split==FALSE)
+
+model<-lm(happiness~income,training)
+summary(model)
+
+test
+pred<-predict(model,test)
+
+install.packages("Metrics")
+library(Metrics)
+rmse(test$happiness,pred)
+
+ggplot()+geom_point(aes(x=training$income,y=training$happiness))+geom_line(aes(x=training$income,y=predict(model,training)))
