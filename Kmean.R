f0a5efb2af09bb4881aad5b9c5843d264c3a7566
Agam Kumar Singh <80572287+Agam-Kumar-Singh@users.noreply.github.com>
2023-01-19 00:06:41 +0530

    Update and rename Clustering.R to Kmean.R

---
+++ Kmean.R
@@ -0,0 +1,18 @@
+libray("ggplot2")
+library("caTools")
+library("ggfortify")
+
+head(iris)
+data<-subset(iris,select=c(1,2,3,4))
+
+wss<-(nrow(data)-1)*(sum(apply(data,2,var)))
+for (i in 2 :15){
+  wss[i]<-sum(kmeans(data,centers = i)$withinss)
+}
+plot(1:15,wss,type="b",xlab="WSS",ylab="Number")
+wss
+
+kmean<-kmeans(data,2)
+kmean$centers
+
+autoplot(kmean,data,frame=TRUE)
